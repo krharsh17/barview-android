dcba5c5b949e6d46317286c7ec44e2977ff1b4f9
Rohit Singhal <rohit.singhal728@gmail.com>
2020-03-01 19:41:59 +0530

    Update BarModel.java
    Co-authored-by: Kumar Harsh <kharsh39@gmail.com>

--- library/src/main/java/in/krharsh17/barview/BarModel.java
+++ library/src/main/java/in/krharsh17/barview/BarModel.java
@@ -4,6 +4,38 @@ public class BarModel {
     String label, value, color;
     Float fillRatio;
 
+    public String getLabel() {
+    	return this.label;
+    }
+
+    public void setLabel(String label) {
+    	this.label = label;
+    }
+
+     public String getValue() {
+    	return this.value;
+    }
+
+    public void setValue(String value) {
+    	this.value = value;
+    }
+
+     public String getColor() {
+    	return this.color;
+    }
+
+    public void setColor(String color) {
+    	this.color = color;
+    }
+
+     public Float getFillRatio() {
+    	return this.fillRatio;
+    }
+
+    public void setFillRatio(Float fillRatio) {
+    	this.fillRatio = fillRatio;
+    }
+
     public BarModel(String label, String value, String color, Float fillRatio) {
         this.label = label;
         this.value = value;
