faebe07dce244550f8c21acbddf6750bd38ed38c
Raghav Awasthi <raghavawasthi2014@gmail.com>
2020-03-01 16:47:43 +0530

    Crash fix

--- library/src/main/java/in/krharsh17/barview/Constants.java
+++ library/src/main/java/in/krharsh17/barview/Constants.java
@@ -3,7 +3,7 @@ package in.krharsh17.barview;
 public interface Constants {
     
     String labelTextColor = "#424242";
-    String valueTextColor = "FFFFFF";
-    String rippleColor = "EEEEEE";
+    String valueTextColor = "#FFFFFF";
+    String rippleColor = "#EEEEEE";
     String charArray = "0123456789ABCDEF";
 }
