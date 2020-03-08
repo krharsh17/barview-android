68c412cf5e10b39be9c1dea33a8795beae2c85fe
Raghav Awasthi <raghavawasthi2014@gmail.com>
2020-03-08 09:32:39 +0530

    f

--- apkscript.bat
+++ apkscript.bat
@@ -1,21 +1,47 @@
-git config --global user.name "Raghav Awasthi"
+IF APPVEYOR_REPO_BRANCH=="develop"(
+	echo "Building a Pull Request"
+git config --global user.name "AutoBot"
 git config --global user.email "raghavawasthi2014@gmail.com"
 
-git clone --quiet --branch=apk https://RaghavAwasthi:$GITHUB_API_KEY@github.com/RaghavAwasthi/barview-android apk > /dev/null
-git status
-cp build/outputs/apk/android-debug.apk apk/test-android-debug.apk
+git clone --quiet --branch=apk https://RaghavAwasthi:$GITHUB_API_KEY@github.com/RaghavAwasthi/barview-android apk 
 cd apk
-cp C:\projects\barview-android\app\build\outputs\apk\app-debug.apk ./test-android-debug.apk
+cp C:\projects\barview-android\app\build\outputs\apk\app-debug.apk ./test-android.apk
 git status
 ls
 git checkout --orphan temporary
 git status
 
-git add test-android-debug.apk
-git commit -am "[Auto] Update Test Apk ($(date +%Y-%m-%d.%H:%M:%S))"
+git add test-android.apk
+git commit -am "[Auto] Update Test Apk (%date% %time%)"
 echo "commit Done"
 
 git branch -D apk
 git branch -m apk
 
-git push origin apk --force --quiet > /dev/null
+git push origin apk --force --quiet 
+) ELSE IF APPVEYOR_REPO_BRANCH=="master" (
+echo "Building a Pull Request on master"
+git config --global user.name "AutoBot"
+git config --global user.email "raghavawasthi2014@gmail.com"
+
+git clone --quiet --branch=apk https://RaghavAwasthi:$GITHUB_API_KEY@github.com/RaghavAwasthi/barview-android apk 
+cd apk
+cp C:\projects\barview-android\app\build\outputs\apk\app-debug.apk ./master-android.apk
+git status
+ls
+git checkout --orphan temporary
+git status
+
+git add master-android.apk
+git commit -am "[Auto] Update Test Apk (%date% %time%)"
+echo "commit Done"
+
+git branch -D masterapk
+git branch -m masterapk
+
+git push origin masterapk --force --quiet 
+)
+ELSE(
+
+	echo "Not a Pull Request"
+	)
