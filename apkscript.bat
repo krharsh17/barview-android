2617ca13b2fe9b70476870ffe0ec05f37d0197b1
Raghav Awasthi <raghavawasthi2014@gmail.com>
2020-03-08 07:59:17 +0530

    b3

--- apkscript.bat
+++ apkscript.bat
@@ -3,3 +3,5 @@ git config --global user.email "raghavawasthi2014@gmail.com"
 
 git clone --quiet --branch=apk https://RaghavAwasthi:$GITHUB_API_KEY@github.com/RaghavAwasthi/barview-android apk > /dev/null
 pwd
+ls
+dir
