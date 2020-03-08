ffb33a10a664f75ee29e0fbef845a350c32e6a77
Raghav Awasthi <raghavawasthi2014@gmail.com>
2020-03-08 09:39:42 +0530

    d

--- apkscript.bat
+++ apkscript.bat
@@ -1,5 +1,5 @@
-IF APPVEYOR_REPO_BRANCH=="develop"(
-	echo "Building a Pull Request"
+echo APPVEYOR_REPO_BRANCH
+echo "Building a Pull Request"
 git config --global user.name "AutoBot"
 git config --global user.email "raghavawasthi2014@gmail.com"
 
@@ -19,29 +19,3 @@ git branch -D apk
 git branch -m apk
 
 git push origin apk --force --quiet 
-) ELSE IF APPVEYOR_REPO_BRANCH=="master" (
-echo "Building a Pull Request on master"
-git config --global user.name "AutoBot"
-git config --global user.email "raghavawasthi2014@gmail.com"
-
-git clone --quiet --branch=apk https://RaghavAwasthi:$GITHUB_API_KEY@github.com/RaghavAwasthi/barview-android apk 
-cd apk
-cp C:\projects\barview-android\app\build\outputs\apk\app-debug.apk ./master-android.apk
-git status
-ls
-git checkout --orphan temporary
-git status
-
-git add master-android.apk
-git commit -am "[Auto] Update Test Apk (%date% %time%)"
-echo "commit Done"
-
-git branch -D masterapk
-git branch -m masterapk
-
-git push origin masterapk --force --quiet 
-)
-ELSE(
-
-	echo "Not a Pull Request"
-	)
