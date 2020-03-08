0f22ac8d8c2ca3a1c898f24af9a3d69b22112f17
Raghav Awasthi <raghavawasthi2014@gmail.com>
2020-03-08 08:16:47 +0530

    b3

--- apkscript.bat
+++ apkscript.bat
@@ -2,6 +2,20 @@ git config --global user.name "Raghav Awasthi"
 git config --global user.email "raghavawasthi2014@gmail.com"
 
 git clone --quiet --branch=apk https://RaghavAwasthi:$GITHUB_API_KEY@github.com/RaghavAwasthi/barview-android apk > /dev/null
-pwd
+git status
+cp build/outputs/apk/android-debug.apk apk/test-android-debug.apk
+cd apk
+cp C:\projects\barview-android\app\build\outputs\apk\app-debug.apk ./test-android-debug.apk
+git status
 ls
-dir
+git checkout --orphan temporary
+git status
+
+git add test-android-debug.apk
+git commit -am "[Auto] Update Test Apk ($(date +%Y-%m-%d.%H:%M:%S))"
+echo "commit Done"
+
+git branch -D apk
+git branch -m apk
+
+git push origin apk --force --quiet > /dev/null
